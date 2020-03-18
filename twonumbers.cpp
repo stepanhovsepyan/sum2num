aaa134eb87cecc04ff5103e7b4c1daf45d6bcf2f
stepanhovsepyan <59359462+stepanhovsepyan@users.noreply.github.com>
2020-03-17 23:20:29 -0700

    Add files via upload

---
+++ twonumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//main method
+int main() {
+	//Step 1:
+	int num_1, num_2;
+	cout << "Please enter the first number: ";
+	cin >> num_1;
+	cout << "Please enter the second number: ";
+	cin >> num_2;
+	//Step 2:
+	int sum;
+	sum = num_1 + num_2;
+	cout << "The sum of the two numbers is: " << sum << endl;
+}
